db1cd8490f0c72670d594e20114fee2734717982
Abhinav P.R <abhinavpr598@gmail.com>
2026-05-29 20:09:11 +0530

    improved ui

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // fetch = function used for making http request tp fetch resources;
 //         (JSON style data, images, files)
 
-
 async function searchPokemon() {
   let pokemonName = document.getElementById("pokemonInput").value.toLowerCase();
 
@@ -10,33 +9,38 @@ async function searchPokemon() {
   );
 
   let data = await response.json();
-  console.log(data)
+  console.log(data);
 
-// IMAGE data
-   const image = data.sprites.front_default
+  // IMAGE data
+  const image = data.sprites.front_default;
 
-   const imageElement = document.getElementById("pokemonSprite")
-   imageElement.src =image
-   imageElement.style.display ="block"
+  const imageElement = document.getElementById("pokemonSprite");
+  imageElement.src = image;
+  imageElement.style.display = "block";
 
+  const name = data.name;
+  const height = data.height;
+  const weight = data.weight;
+  const type1 = data.types[0].type.name;
+  const type2 = data.types[1]?.type.name || "";
 
-   const name = data.name;
-   const height = data.height;
-   const weight = data.weight;
-   const type = data.types[0].type.name
+  //Abilities data
+  const ability1 = data.abilities[0].ability.name
+  const ability2 = data.abilities[1]?.ability.name
+  const ability3 = data.abilities[2]?.ability.name
 
-
-// Data : type, name, height, weight
+  // Data : type, name, height, weight
   document.getElementById("pokemonDetails").innerHTML =
     `<div class="pokemonInformation">
       <h2>Name : ${name}</h2>
-      <P>Type : ${type}</p>
+      <P>Primary Type : ${type1} </p>
+      <P>Secondary Type : ${type2}</P>
       <p>Height :${height}</p>
       <p>Weight : ${weight}</p>
+      <p> ability1 : ${ability1}</p>
+      <p> ability2 : ${ability2}</p>
+      <p> ability3 : ${ability3}</p>
    </div>`;
-
-
-   
 
   document.getElementById("pokemonInput").value = "";
 }
