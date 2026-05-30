0b2afd113be92861395e7a3405ebc8f0966347b9
Abhinav P.R <abhinavpr598@gmail.com>
2026-05-30 20:17:27 +0530

    created ai generated card section

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // fetch = function used for making http request tp fetch resources;
 //         (JSON style data, images, files)
 
+let pokemonData;
+
 async function searchPokemon() {
   let pokemonName = document.getElementById("pokemonInput").value.toLowerCase();
 
@@ -9,7 +11,7 @@ async function searchPokemon() {
   );
 
   let data = await response.json();
-  console.log(data);
+  pokemonData = data;
 
   // IMAGE data
   const image = data.sprites.front_default;
@@ -25,9 +27,9 @@ async function searchPokemon() {
   const type2 = data.types[1]?.type.name || "";
 
   //Abilities data
-  const ability1 = data.abilities[0].ability.name
-  const ability2 = data.abilities[1]?.ability.name
-  const ability3 = data.abilities[2]?.ability.name
+  const ability1 = data.abilities[0].ability.name;
+  const ability2 = data.abilities[1]?.ability.name;
+  const ability3 = data.abilities[2]?.ability.name;
 
   // Data : type, name, height, weight
   document.getElementById("pokemonDetails").innerHTML =
@@ -37,10 +39,26 @@ async function searchPokemon() {
       <P>Secondary Type : ${type2}</P>
       <p>Height :${height}</p>
       <p>Weight : ${weight}</p>
-      <p> ability1 : ${ability1}</p>
-      <p> ability2 : ${ability2}</p>
-      <p> ability3 : ${ability3}</p>
+      <p>Abilities : ${ability1} , ${ability2} , ${ability3}</P>
    </div>`;
 
   document.getElementById("pokemonInput").value = "";
+}
+
+
+function generateCard(){
+  document.getElementById("pokemon-app__ai-card").innerHTML = `
+  <div class="pokemonAiCard">
+
+  <img src="${pokemonData.sprites.front_default}">
+  
+  <h2>${pokemonData.name}</h2>
+
+  <p>${pokemonData.types[0].type.name}</P>
+
+  <P>A powerful pokemon known for its ${pokemonData.abilities[0].ability.name} ability.</P>
+  
+  
+  </div>`
+  
 }
